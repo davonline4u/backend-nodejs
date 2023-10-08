e049878954b6b38c6d884a7e74a49f3725b1f288
davonline4u <35406730+davonline4u@users.noreply.github.com>
2023-10-07 20:43:21 -1200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose, {mongo} from "mongoose";
 
 const connectDB = async () => {
     try{
-        const conn = await mongoose.connect(process.env.MONGO_URI);
+        const conn = await mongoose.connect("mongodb+srv://anambra:genesis@cluster0.tquawga.mongodb.net/anambra-marriage?retryWrites=true&w=majority");
         console.log(`Mongo Db Connected : ${conn.Connection}`);
     }catch(error) {
         console.error(`Error: ${error.message}`)
